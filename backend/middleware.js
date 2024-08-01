bcb3e33caec15385c9bbb477fc46a71a5c3e75e6
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-08-01 13:34:17 +0530

    removed log statements

--- backend/middleware.js
+++ backend/middleware.js
@@ -11,7 +11,6 @@ const authMiddleware = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        console.log('Decoded Token:', decoded);
         if (decoded.userId) {
             req.userId = decoded.userId;
             next();
