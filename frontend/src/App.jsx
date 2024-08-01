73471338dd43e1e719379b287f958009e88a0c29
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-08-01 18:54:50 +0530

    created the dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, BrowserRouter, Route } from "react-router-dom"
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
           <Route path="/signup" element={ <Signup />} />
           <Route path="/signin" element={ <Signin />} />
           <Route path="/dashboard" element={ <Dashboard />} />
-          {/* <Route path="/send" element={ <SendMoney />} /> */}
+          <Route path="/send" element={ <SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/SendMoney.jsx
@@ -0,0 +1,5 @@
+export const SendMoney = () => {
+    return <div className="flex justify-center h-screen bg-gray-100">
+        <div></div>
+    </div>
+}
