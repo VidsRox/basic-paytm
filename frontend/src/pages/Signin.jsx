1e37494465f0497b63bc57ee52632a8b97acc74b
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-08-14 16:36:45 +0530

    imports fixed

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { BottomWarning } from "../components/BottomWarning";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
 import axios from "axios";
 import { useAuth } from "../authentication/AuthContext";
-import Heading from "../components/Heading";
-import SubHeading from "../components/SubHeading";
-import InputBox from "../components/InputBox";
-import Button from "../components/Button";
-import BottomWarning from "../components/BottomWarning";
 
 export const Signin = () => {
     const [username, setUsername] = useState("");
@@ -48,14 +48,12 @@ export const Signin = () => {
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
             {/* Home Page Button */}
-            <div className="absolute top-4 left-4">
-                <button
-                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-                    onClick={() => navigate("/")}
-                >
-                    Home Page
-                </button>
-            </div>
+            <button
+                onClick={() => navigate("/")}
+                className="absolute top-4 left-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            >
+                Home Page
+            </button>
 
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
